88cdc538fbda7170e48fd4f7ba66f70d3c0ec0b9
Fredrik Sellgren <frse1700@student.miun.se>
2019-08-22 11:44:54 +0200

    Initial Commit

---
+++ CpuSpeed/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CpuSpeed
+//
+//  Created by Fredrik Sellgren on 2019-08-22.
+//  Copyright © 2019 Fredrik Sellgren. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CpuSpeed/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CpuSpeed
+//
+//  Created by Fredrik Sellgren on 2019-08-22.
+//  Copyright © 2019 Fredrik Sellgren. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
